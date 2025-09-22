16a1921ba016e000a8a9d6f35395c4d896ce5f9c
olaholunuga <olaholunuga@gmail.com>
2025-09-22 14:28:31 +0100

    implementing the render class

---
+++ frontends/console/renderers.py
@@ -0,0 +1,38 @@
+import textwrap
+from typing import Iterable
+
+from tic_tac_toe.game.renderers import Renderer
+from tic_tac_toe.logic.models import GameState
+
+class ConsoleRenderer(Renderer):
+
+    def render(self, game_state: GameState) -> None:
+        clear_screen()
+        if game_state.winner:
+            print_blinking(game_state.grid.cells, game_state.winning_cells)
+            print(f"{game_state.winner} wins \N{party popper}")
+        else:
+            print_solid(game_state.grid.cells)
+            if game_state.tie:
+                print("No one wins this time \N{neutral face}")
+
+def clear_screen() -> None:
+    print("\033c", end="")
+
+def blink(text: str) -> str:
+    return f"\033[5m{text}\033[0m"
+
+def print_solid(cells: Iterable[str]) -> None:
+    print(
+        textwrap.indent(
+            """\
+             A   B   C
+           ------------
+        1 ┆  {0} │ {1} │ {2}
+          ┆ ───┼───┼───
+        2 ┆  {3} │ {4} │ {5}
+          ┆ ───┼───┼───
+        3 ┆  {6} │ {7} │ {8}
+    """
+        )
+    )
