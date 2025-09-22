e6ba8f6faecc284cbaf266088e2fae37a3e9c019
olaholunuga <olaholunuga@gmail.com>
2025-09-22 13:13:58 +0100

    implemented the plyers make_move method and the get_move abstract method

---
+++ library/src/tic_tac_toe/game/players.py
@@ -0,0 +1,21 @@
+import abc
+
+from tic_tac_toe.logic.exceptions import InvalidMove
+from tic_tac_toe.logic.models import Mark, GameState, Move
+
+class Player(metaclass=abc.ABCMeta):
+    def __init__(self, mark: Mark) -> None:
+        self.mark = mark
+    
+    def make_move(self, game_state: GameState) -> GameState:
+        if self.mark is game_state.current_mark:
+            if move := self.get_move(game_state):
+                return move.after_state
+            raise InvalidMove("No more possible moves")
+        else:
+            raise InvalidMove("It's the other player's turn")
+    
+    @abc.abstractmethod
+    def get_move(self, game_state: GameState) -> Move | None:
+        """Return the current player's move in the given game state.
+        """
