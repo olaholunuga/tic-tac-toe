83923c8a45de2a0e7a591c9c5b5da4c42e355aa5
olaholunuga <olaholunuga@gmail.com>
2025-09-22 11:35:50 +0100

    imple mented the InvalidMove Exception

--- library/src/tic_tac_toe/logic/exceptions.py
+++ library/src/tic_tac_toe/logic/exceptions.py
@@ -2,3 +2,5 @@
 class InvalidGameState(Exception):
     """Raised when the game state is invalid
     """
+class InvalidMove(Exception):
+    """Raised when a player makes an invalid move to any occupied position"""
