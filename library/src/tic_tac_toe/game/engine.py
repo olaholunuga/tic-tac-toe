b83407f37d1d5b8f893003de487fdf5e3b165dfb
olaholunuga <olaholunuga@gmail.com>
2025-09-22 12:52:51 +0100

    getting current player and validating them

--- library/src/tic_tac_toe/game/engine.py
+++ library/src/tic_tac_toe/game/engine.py
@@ -4,12 +4,16 @@ from tic_tac_toe.game.players import Player
 from tic_tac_toe.game.renderers import Renderer
 from tic_tac_toe.logic.exceptions import InvalidMove
 from tic_tac_toe.logic.models import GameState, Grid, Mark
+from tic_tac_toe.logic.validators import validate_players
 
 @dataclass(frozen=True)
 class TicTacToe:
     player1: Player
     player2: Player
     renderer: Renderer
+
+    def __post_int__(self):
+        validate_players(self.player1, self.player2)
 
     def play(self, starting_mark: Mark = Mark("X")) -> None:
         game_state = GameState(Grid(), starting_mark)
@@ -22,3 +26,9 @@ class TicTacToe:
                 game_state = player.make_move(game_state)
             except InvalidMove:
                 pass
+    
+    def get_current_player(self, game_state: GameState) -> Player:
+        if game_state.current_mark is self.player1.mark:
+            return self.player1
+        else:
+            return self.player2
