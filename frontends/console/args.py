7027a93c18134b1c9dedfd3a36381466e66ee067
olaholunuga <olaholunuga@gmail.com>
2025-09-23 13:18:04 +0100

    Added the minimax Player to the console frontend

--- frontends/console/args.py
+++ frontends/console/args.py
@@ -2,13 +2,18 @@ import argparse
 from typing import NamedTuple
 
 from tic_tac_toe.logic.models import Mark
-from tic_tac_toe.game.players import Player, RandomComputerPlayer
+from tic_tac_toe.game.players import (
+    Player, 
+    RandomComputerPlayer,
+    MinimaxComputerPlayer,
+)
 
 from .players import ConsolePlayer
 
 PLAYER_CLASSES = {
     "human": ConsolePlayer,
     "random": RandomComputerPlayer,
+    "minimax": MinimaxComputerPlayer,
 }
 
 class Args(NamedTuple):
@@ -28,7 +33,7 @@ def parse_args() -> Args:
         "-O",
         dest="player_o",
         choices= PLAYER_CLASSES.keys(),
-        default="random",
+        default="minimax",
     )
     parser.add_argument(
         "--starting",
